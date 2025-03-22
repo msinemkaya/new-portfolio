ee7d9087bef64cd93994f07603570ffe7c6a2104
Melike Sinem Kaya <msinemkaya6@gmail.com>
2025-03-23 00:28:33 +0300

    feat: github pages

--- src/components/modules/Navbar.jsx
+++ src/components/modules/Navbar.jsx
@@ -6,7 +6,7 @@ export function Navbar() {
     <div className="flex gap-6 items-center fixed top-12 right-12 z-50">
       <NavLink to="about">ABOUT ME</NavLink>
       <NavLink to="myWork">MY WORK</NavLink>
-      <NavLink resume to="/resume.pdf" target="_blank">
+      <NavLink resume to={`${import.meta.env.BASE_URL}resume.pdf`} target="_blank">
         <div className="flex gap-2 items-center">
           RESUME <HiOutlineLink/>
         </div>
